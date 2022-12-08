d0bac877541afef95cebef0f1da10a7d78bc4044
Jacques-Portable <daxxas158@gmail.com>
2022-12-08 15:24:39 +0100

    placeholder moving cart

---
+++ Assets/Scripts/Player/CartController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using PathCreation;
+using UnityEngine;
+
+public class CartController : MonoBehaviour
+{
+    [SerializeField] private PathCreator path;
+    
+    [SerializeField] private float cartSpeed = 0f;
+    [SerializeField] private Quaternion rotationOffset = Quaternion.identity;
+    [SerializeField] private Vector3 positionOffset;
+    private float distanceTravelled = 0f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        distanceTravelled += Time.deltaTime;
+        transform.position = path.path.GetPointAtDistance(distanceTravelled) + positionOffset;
+        transform.rotation = path.path.GetRotationAtDistance(distanceTravelled) * rotationOffset;
+    }
+}
