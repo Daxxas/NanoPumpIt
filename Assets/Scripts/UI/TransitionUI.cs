88ddaaf07f161c16a826cedadd33193c51febc4b
Jacques-Portable <daxxas158@gmail.com>
2022-12-13 10:44:15 +0100

    transitionUI

---
+++ Assets/Scripts/UI/TransitionUI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MoreMountains.Feedbacks;
+using UnityEngine;
+
+namespace UI
+{
+    public class TransitionUI : MonoBehaviour
+    {
+        [SerializeField] private MMF_Player hideTransition;
+        [SerializeField] private MMF_Player displayTransition;
+
+        public void Transition(bool on, Action onComplete)
+        {
+            if (!on)
+            {
+                hideTransition.PlayFeedbacks();
+                hideTransition.Events.OnComplete.AddListener(() => onComplete?.Invoke());
+            }
+            else
+            {
+                displayTransition.PlayFeedbacks();
+                displayTransition.Events.OnComplete.AddListener(() => onComplete?.Invoke());
+            }
+        }
+    }
+}
