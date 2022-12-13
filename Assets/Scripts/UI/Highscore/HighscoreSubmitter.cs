4b56d1cb07957dd00ebf818d74f5fe7db6d6050b
Jacques-Portable <daxxas158@gmail.com>
2022-12-13 16:26:54 +0100

    push player cart save

---
+++ Assets/Scripts/UI/Highscore/HighscoreSubmitter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+public class HighscoreSubmitter : MonoBehaviour
+{
+    [SerializeField] private List<LetterSelector> letters = new List<LetterSelector>();
+    [SerializeField] private Timer timer;
+    [SerializeField] private HighscoreManager highscoreManager;
+    
+    public void SubmitScore()
+    {
+        string name = "";
+        foreach (var letter in letters)
+        {
+            name += letter.CurrentLetter;
+        }
+
+        float time = timer.DefaultTime - timer.GetTime();
+
+        highscoreManager.RegisterNewHighscore(time, name);
+    }
+}
